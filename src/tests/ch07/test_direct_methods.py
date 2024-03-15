6464f01539d0f3432d90cc640f61d82fd1b91156
Griffin Holt <griffinbholt@gmail.com>
2024-03-15 00:59:08 -0700

    Wrote tests for Chapter 7.

--- src/tests/ch07/test_direct_methods.py
+++ src/tests/ch07/test_direct_methods.py
@@ -1,20 +1,40 @@
 import sys; sys.path.append('./src/'); sys.path.append('../../')
 
-from ch07 import cyclic_coordinate_descent, powell, hooke_jeeves, generalized_pattern_search, nelder_mead
+import numpy as np
 
+from ch07 import cyclic_coordinate_descent, powell, hooke_jeeves, generalized_pattern_search, nelder_mead
+from TestFunctions import booth, wheeler
 
 class TestDirectMethods():
+    eps = 1e-5
+
     def test_cyclic_coord_descent(self):
-        pass  # TODO
+        f_min, x_min = booth.global_min()
+        x = np.array([10.0, -5.0])
+        x = cyclic_coordinate_descent(booth, x, self.eps)
+        assert np.abs(booth(x) - f_min) < self.eps
+        assert np.all(np.abs(x - x_min) < self.eps)
 
     def test_cyclic_coord_descent_with_accel(self):
-        pass  # TODO
+        f_min, x_min = booth.global_min()
+        x = np.array([10.0, -5.0])
+        x = cyclic_coordinate_descent(booth, x, self.eps, with_acceleration=True)
+        assert np.abs(booth(x) - f_min) < self.eps
+        assert np.all(np.abs(x - x_min) < self.eps)
 
     def test_powell(self):
-        pass  # TODO
+        f_min, x_min = booth.global_min()
+        x = np.array([10.0, -5.0])
+        x = powell(booth, x, self.eps)
+        assert np.abs(booth(x) - f_min) < self.eps
+        assert np.all(np.abs(x - x_min) < self.eps)
 
     def test_hooke_jeeves(self):
-        pass  # TODO
+        f_min, x_min = wheeler.global_min()
+        x = np.array([0.7, 0.9])
+        x = hooke_jeeves(wheeler, x, alpha=0.5, eps=self.eps, gamma=0.5)
+        assert np.abs(wheeler(x) - f_min) < self.eps
+        assert np.all(np.abs(x - x_min) < self.eps)
 
     def test_generalized_pattern_search(self):
         pass  # TODO
